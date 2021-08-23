823baaebd16276c13a4e1f9fb998bedb8a0621af
NewryBenson <76691448+NewryBenson@users.noreply.github.com>
2021-08-23 18:37:07 +0200

    effects and items

---
+++ Brackeys GameJam aug 2021 v2/Assets/Scripts/EffectsAndItems.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectsAndItems : MonoBehaviour
+{
+    // all effects
+    public bool wet;
+    public bool weapon;
+    public bool poison;
+    public int poisonCount = 1;
+    public int frostCount = 1;
+    public int wetCount = 1;
+    public bool frost;
+    public bool gunpowder;
+    public bool electricity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        wet = false;
+        weapon = false;
+        poison = false;
+        frost = false;
+        gunpowder = false;
+        electricity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
